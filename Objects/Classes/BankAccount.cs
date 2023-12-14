3b901815b5b62dd4bccf256da32b66b532718742
Nicolas Carrasco <nicocarrasco703@outlook.com>
2023-12-14 19:49:28 -0300

    Init commit

---
+++ Objects/Classes/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes;
+
+public class BankAccount
+{
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+    }
+
+    public BankAccount(string name, decimal initBalance)
+    {
+        this.Owner = name; // this solo se requiere cuando una variable local o parametro tiene el mismo nombre
+        this.Balance = initBalance;
+    }
+}
